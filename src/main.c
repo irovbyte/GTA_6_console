e17f344a5ab29174fc15e840cb1cec28f3c8a7c0
RagnaR <shaxnurmurodov1990@gmail.com>
2025-12-16 20:52:39 +0500

    v0.0.2

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "welcome.h"
+
+int main(){
+    welcomeContekst();
+    return 0;
+}
---
+++ src/welcome.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "welcome.h"
+
+void welcomeContekst(){
+    printf("\033[1;33m########################################\n");
+    printf("#                                      #\n");
+    printf("#   ДОБРО ПОЖАЛОВАТЬ В ТЕРМИНАЛЬНУЮ    #\n");
+    printf("#               ШКОЛУ                  #\n");
+    printf("#                                      #\n");
+    printf("########################################\033[0m\n");
+
+    
+}
+
+// void chooseVariants(){
+//     printf("начать игру\n");
+//     printf("============\n");
+//     printf("наркотики\n");
+
+// }
+
+// void settings(){
+//     printf("============\n");
+//     printf("настройка графики\n");
+//     printf("============\n");
+//     printf("настройка звука");
+// }
